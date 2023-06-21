18f4e3d3a3acbd26177bdb63837a49dd0093c5df
Chistry <christian.diaz@correo.unimet.edu.ve>
2023-06-20 22:22:25 -0400

    input parte 1

--- src/hotelbookingsystem/HotelBookingSystem.java
+++ src/hotelbookingsystem/HotelBookingSystem.java
@@ -4,6 +4,11 @@
  */
 package hotelbookingsystem;
 
+import hotelbookingsystem.data.CustomerData;
+import hotelbookingsystem.utils.HashTable;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author Escal
@@ -13,8 +18,26 @@ public class HotelBookingSystem {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("Hello ");
-    }
     
+    public static void main(String[] args) {
+        CustomerData customerdata = new CustomerData();
+        HashTable habitaciones = customerdata.getHabitaciones();
+        
+        Scanner scanner = new Scanner(System.in);
+        String fullname;
+
+        System.out.println("Enter the guest´s fullname to search for the room:");
+        fullname = scanner.nextLine();
+
+        if (habitaciones.get(fullname)==null){
+            System.out.println(fullname +"'s room does not exist, contact customer service.");
+        }
+        else{
+            System.out.println(fullname +"'s room is number: " + habitaciones.get(fullname)+".");
+        }
+        
+    
+    }
 }
+    
+
